3548bcb5b02bff45f2502fd930ed656878acbe28
Joy M <creator@Life-Is-Joy.local>
2021-09-19 20:45:47 +0900

    ADD: testing react component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react"
+import Tomato from "./tomato"
 
 function App() {
   return (
     <div className="App">
      it's working!!! 
+     <Tomato />
     </div>
   );
 }
---
+++ src/tomato.js
@@ -0,0 +1,9 @@
+function Tomato(){
+    return (
+        <div>
+            <h1>I love tomato!!</h1>
+        </div>
+    );
+}
+
+export default Tomato;
