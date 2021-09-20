f580b6aa4c857a6c0f8509a1b76d67ccb8eea924
Joy M <creator@Life-Is-Joy.local>
2021-09-20 23:03:10 +0900

    ADD: rendering the movies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from "react"
-import PropTypes from "prop-types";
+import axios from "axios"
+import Movie from "./Movie"
 
 
 class App extends React.Component{
   state = {
-    count: 0
-  };
-  add = () => { console.log("ADD")};
-  minus = () =>{ console.log("Minus")}
-
-  render(){
-    return (
-      <div>
-        <h1>The Number is: {this.state.counte}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    )
+    isLoading: true,
+    movies:[]
   }
+
+  getMovies = async () =>{
+ const {data:{data:{movies}}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating")
+ this.setState({movies, isLoading:false})
+  
 }
 
-
+  componentDidMount(){
+    this.getMovies();
+  }
+  render(){
+    const {isLoading,movies} = this.state;
+    return (<div> {isLoading ? "Loading...": movies.map(movie => (
+       <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image}/>
+      ))}
+    </div>
+    );
+  }
+}
 
 
 export default App;
